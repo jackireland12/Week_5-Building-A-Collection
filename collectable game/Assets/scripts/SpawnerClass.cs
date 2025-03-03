2f3ccdf8709accbbfd0b4d8359400d7f02675a19
jackireland2 <121182167+jackireland2@users.noreply.github.com>
2025-03-03 01:45:58 +0000

    waves

--- collectable game/Assets/scripts/SpawnerClass.cs
+++ collectable game/Assets/scripts/SpawnerClass.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnerClass : MonoBehaviour
@@ -8,6 +9,7 @@ public class SpawnerClass : MonoBehaviour
     public GameObject enemie1;
     public List<GameObject> enemineType;
     public int waveNumber = 1;
+    private bool isSpawing = false;
 
     private void Update()
     {
@@ -21,13 +23,39 @@ public class SpawnerClass : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            deleteEnemie();
+            StartWaves();
         }
+        
         CleanEnemyList();
+        if (enemies.Count == 0&& isSpawing == false)
+        {
+            StartWaves();
+        }
         
     }
-    void Waves()
+    IEnumerator spawnWaves(int count)
     {
+        for(int i = 0;i < count; i++)
+        {
+            
+            int RandomEnemie = Random.Range(0, enemineType.Count);
+            GameObject choisenEnime = enemineType[RandomEnemie];
+            Vector3 randomspawn = new Vector3(5.0f, Random.Range(-5.0f, 5.0f), 0f);
+            yield return new WaitForSeconds(0.3f);
+            GameObject enemy = Instantiate(choisenEnime, randomspawn, Quaternion.identity);
+            enemies.Add(enemy);
+        }
+        isSpawing = false ;
+       
+
+    }
+
+    void StartWaves()
+    {
+        isSpawing = true;
+        waveNumber++;
+        int enemiesCount = Mathf.RoundToInt(5 + waveNumber *3);
+        StartCoroutine( spawnWaves(enemiesCount));
 
     }
 
